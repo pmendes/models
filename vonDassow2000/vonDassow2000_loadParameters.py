abe7dbea9967602dc21e8b0c115033c83e49d96d
Pedro Mendes <mendes@copasi.org>
2023-03-02 14:19:18 -0500

    better version checks; now adds only parameters present (datafile can have less than 48)

--- vonDassow2000/vonDassow2000_loadParameters.py
+++ vonDassow2000/vonDassow2000_loadParameters.py
@@ -10,6 +10,7 @@
 
 import sys
 import csv
+import re
 from pathlib import Path
 if '../..' not in sys.path:
     sys.path.append('../..')
@@ -20,6 +21,9 @@ import time
 from datetime import date
 #import matplotlib.pyplot as plt
 #%matplotlib inline
+
+# highest version that this file can deal with
+mversion = 3
 
 # check arguments
 n = len(sys.argv)
@@ -42,13 +46,18 @@ else:
 load_model(modelfile)
 
 # check that we can work with this model version
-try:
-    model_info.get_notes().index("version: 1.")
-except:
-    print("\nThe model does not seem to be a compatible model created by vonDassow2000.py\n")
+vrs = re.search(r"<p>version: (\d+)\.\d+x\d+", model_info.get_notes())
+# if no version then we quit
+if( vrs==None ):
+    print(f'\n{modelfile} does not seem to be a compatible model created by vonDassow2000.py\n')
     exit()
+fversion = int(vrs.group(1))
+# give warnings if version is outside our domain
+if fversion < 1:
+    print(f'\n{modelfile} has a strange version number ({fversion}), this may not work well...\n')
+if fversion > 3:
+    print(f'\n{modelfile} was created with a version more recent that this file ({fversion} > {mversion}), this may not work well...\n')
 
-# TODO: the strategy below needs to be tested with files that contain some columns missing
 # load the parameter set data file
 index = 0
 with open(datafile) as fd:
@@ -56,7 +65,8 @@ with open(datafile) as fd:
     for row in psets:
         # set each of the parameters by name
         for par in {'H_ci', 'H_wg', 'H_EWG', 'H_PTC', 'H_IWG', 'H_PH', 'H_HH', 'H_ptc', 'H_hh', 'H_en', 'H_EN', 'H_CI', 'kappa_CIwg', 'kappa_Bci', 'kappa_PTCHH', 'kappa_ENhh', 'kappa_CNptc', 'kappa_CIptc', 'kappa_WGen', 'kappa_PTCCI', 'nu_WGen', 'kappa_CNhh', 'kappa_CNwg', 'kappa_WGwg', 'kappa_CNen', 'kappa_ENci', 'nu_CNwg', 'nu_PTCCI', 'nu_CNen', 'nu_WGwg', 'nu_ENhh', 'nu_CNhh', 'nu_CIptc', 'nu_ENci', 'nu_CIwg', 'nu_CNptc', 'nu_Bci', 'alpha_WGwg', 'alpha_CIwg', 'r_LMxferPTC', 'r_LMxferHH', 'r_ExoWG', 'r_MxferWG', 'r_LMxferWG', 'r_EndoWG', 'C_CI', 'PTC_0', 'HH_0'}:
-            set_parameters(name=par, exact=True, initial_value= row[par])
+            if par in row:
+                set_parameters(name=par, exact=True, initial_value= row[par])
         index += 1
         # add this as a parameter set
         add_parameter_set(f'{setname} {index}')
